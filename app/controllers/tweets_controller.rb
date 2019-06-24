3f34d31ce11dcddb6020000e90d52c5b30f3c69a
田中　太朗 <groveleffect@gmail.com>
2019-06-24 12:21:34 +0000

    新規投稿ページの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,15 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+  
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+    @tweet =Tweet.new(params.require(:tweet).permit(:content))
+    if @tweet.save
+      redirect_to tweets_path
+    end
+  end
 end
