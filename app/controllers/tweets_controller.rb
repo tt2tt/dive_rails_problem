7f9a23ee2338a03fd1884e9b91da93351e34d830
田中　太朗 <groveleffect@gmail.com>
2019-06-26 11:25:30 +0000

    Flashメッセージの追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,7 @@ class TweetsController < ApplicationController
   def create
     @tweet =Tweet.new(params.require(:tweet).permit(:content))
     if @tweet.save
-      redirect_to tweets_path
+      redirect_to tweets_path, notice:"Tweetを投稿しました！"
     end
   end
   
@@ -21,14 +21,14 @@ class TweetsController < ApplicationController
   def update
     @tweet = Tweet.find(params[:id])
     if @tweet.update(params.require(:tweet).permit(:content))
-      redirect_to tweets_path
+      redirect_to tweets_path, notice:"Tweetを更新しました！"
     end
   end
   
   def destroy
     @tweet = Tweet.find(params[:id])
     if @tweet.delete
-      redirect_to tweets_path
+      redirect_to tweets_path, notice:"Tweetを削除しました！"
     end
   end
 end
