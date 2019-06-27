2585de5e224936f194e810927e6a44555d4bebc1
田中　太朗 <groveleffect@gmail.com>
2019-06-27 05:18:49 +0000

    確認ページに戻るボタンの追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,11 @@ class TweetsController < ApplicationController
   end
   
   def new
-    @tweet = Tweet.new
+    if params[:back]
+      @tweet = Tweet.new(tweet_params)
+    else
+      @tweet = Tweet.new
+    end
   end
   
   def create
