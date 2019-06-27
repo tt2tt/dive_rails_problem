a0bfa656887fef559c7903408780e8a382be28b5
田中　太朗 <groveleffect@gmail.com>
2019-06-27 04:58:06 +0000

    新規投稿時のみ確認画面を表示させるヘルパーメソッドの作成

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,9 @@
 module TweetsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'confirm'
+      confirm_tweets_path
+    elsif action_name == 'edit'
+      blog_path
+    end
+  end
 end
